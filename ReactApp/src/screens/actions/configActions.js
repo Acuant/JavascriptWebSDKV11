2128fc1ecd96413331b4b8bde613c73a5d6300ed
Tommy Han <tommy_han1@intuit.com>
2021-08-12 15:46:32 -0400

    added user/password

--- ReactApp/src/screens/actions/configActions.js
+++ ReactApp/src/screens/actions/configActions.js
@@ -10,7 +10,7 @@ export function setInstanceID(payload) {
                 })
             })
             .catch(err => {
-                console.log(err)
+                console.log("ApiService.setInstanceID:", err)
                 throw new Error(err);
             })
 
